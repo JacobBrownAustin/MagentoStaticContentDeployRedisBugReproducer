7fb2e4e59074ecac7138ce48ac893f3e2855ce79
Jacob Brown <jacob@gnu.org>
2018-01-30 23:55:47 -0600

    Cleaning up.

--- Plugin/PackagePlugin.php
+++ Plugin/PackagePlugin.php
@@ -7,36 +7,12 @@
 
 namespace Magento\StaticContentDeployRedisBugReproducer\Plugin;
 
-use Magento\Framework\App\Cache\Manager as CacheManager;
 use Magento\Deploy\Package\Package;
 use Magento\Framework\App\CacheInterface;
-use Magento\Framework\App\Cache;
-use Magento\Framework\Config\CacheInterface as ConfigCacheInterface;
-use Magento\Framework\App\Cache\Frontend\Factory as CacheFrontendFactory;
 use Magento\Framework\App\Cache\Frontend\Pool as CacheFrontendPool;
 
 class PackagePlugin
 {
-
-  /**
-    * @var CacheManager
-    */
-   protected $cacheManager;
-
-  /**
-    * @var CacheInterface
-    */
-   protected $cacheInterface;
-
-  /**
-    * @var ConfigCacheInterface
-    */
-   protected $configCacheInterface;
-
-  /**
-    * @var CacheFrontendFactory
-    */
-   protected $cacheFrontendFactory;
 
   /**
     * @var CacheFrontendPool
@@ -44,24 +20,11 @@ class PackagePlugin
    protected $cacheFrontendPool;
 
    /**
-    * @param CacheManager $cacheManager
-    * @param CacheInterface $cacheInterface
-    * @param ConfigCacheInterface $configCacheInterface
-    * @param CacheFrontendFactory $cacheFrontendFactory
     * @param CacheFrontendPool $cacheFrontendPool
     */
    public function __construct(
-       CacheManager $cacheManager,
-       CacheInterface $cacheInterface,
-       ConfigCacheInterface $configCacheInterface,
-       CacheFrontendFactory $cacheFrontendFactory,
        CacheFrontendPool $cacheFrontendPool
    ) {
-       $this->cacheManager = $cacheManager;
-       $this->cacheInterface = $cacheInterface;
-       $this->configCacheInterface = $configCacheInterface;
-       #$this->frontendCache = \Zend_Cache::factory( "Cm_Cache_Backend_Redis",
-       $this->cacheFrontendFactory = $cacheFrontendFactory;
        $this->cacheFrontendPool = $cacheFrontendPool;
    }
 
@@ -71,11 +34,8 @@ class PackagePlugin
             echo("beforeGetFiles called from pid: " . getmypid() . "\n");
         }
 		$startTime = time();
-        # nope # $frontendinterface = $this->cacheFrontendFactory->create();
         $frontendinterface = $this->cacheFrontendPool->get("default");
         while ( time() < $startTime + 5) {
-		    # nope # $this->cacheInterface->load("blah-blah-blah");
-            # nope # $blah = $this->configCacheInterface->load("global::DiConfig");
             $data = $frontendinterface->load("global::DiConfig");
 			$test = true;
         }
